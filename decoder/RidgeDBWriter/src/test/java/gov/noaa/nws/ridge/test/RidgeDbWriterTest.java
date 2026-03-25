c636818ea8b38b4d6903198719a795daeea87144
akrherz <akrherz@iastate.edu>
2026-03-24 23:46:47 -0500

    💚 Fix build

--- decoder/RidgeDBWriter/src/test/java/gov/noaa/nws/ridge/test/RidgeDbWriterTest.java
+++ decoder/RidgeDBWriter/src/test/java/gov/noaa/nws/ridge/test/RidgeDbWriterTest.java
@@ -9,16 +9,20 @@ import gov.noaa.nws.ridge.RidgeTimeDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit38.AbstractTransactionalJUnit38SpringContextTests;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.fail;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.PrecisionModel;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.PrecisionModel;
 
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/ridgeDBWriterSpring.xml"})
-public class RidgeDbWriterTest extends AbstractTransactionalJUnit38SpringContextTests {
+public class RidgeDbWriterTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Autowired
 	@Qualifier("radarDAO")
